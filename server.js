a2776e174c94b5cb0da31371fa4810349fc10467
Masselot Guillaume <guillaume.masselot@gmail.com>
2019-04-21 11:14:26 +0200

    env.port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 // Configuring the database
 const config = require('./server/config.js');
@@ -41,6 +41,6 @@ app.get('/*', function (req, res) {
 });
 
 // listen on port 3000
-app.listen(config.serverport, () => {
+app.listen(process.env.PORT || config.serverport, () => {
     console.log("Server is listening on port 3000");
 });
